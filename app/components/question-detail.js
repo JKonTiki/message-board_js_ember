3226a1c5f24bd55d2f653982eb10894380042ff2
Jeremy Fryd <fryd21@gmail.com>
2016-10-28 09:58:49 -0700

    up and running detail page

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:{
 
-  delete(question){
-    if(confirm("Are you s-s-shh-shhh-SUUUREUEREE!?!?!?!?!")){
-      this.sendAction('destroyQuestion', question)
+    delete(question){
+      if(confirm("Are you s-s-shh-shhh-SUUUREUEREE!?!?!?!?!")){
+        this.sendAction('destroyQuestion', question)
+      }
     }
-  }
 
+  }
 });
